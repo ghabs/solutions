6183841a9d849226296416aa12864e16e47bbf50
Ben Goldhaber <goldhaber.ben@gmail.com>
2017-11-16 11:33:48 -0800

    all your base

---
+++ c/all-your-base/src/all_your_base.h
@@ -0,0 +1,10 @@
+#ifndef ALL_YOUR_BASE_H
+#define ALL_YOUR_BASE_H
+
+#define DIGITS_ARRAY_SIZE 64
+#include <stddef.h>
+#include <stdint.h>
+
+size_t rebase(int8_t * digits, int16_t base, int16_t outputbase, size_t inputlength);
+
+#endif
